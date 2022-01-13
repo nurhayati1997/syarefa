9125d89c9993ffa661ae3c07445496225205fc55
nurhayati1997 <nurhayatifitriani77@gmail.com>
2022-01-13 23:21:03 +0700

    remake counter bpjs

--- application/views/bpjs_v.php
+++ application/views/bpjs_v.php
@@ -20,6 +20,8 @@
     <link rel="stylesheet" href="assets/css/templatemo-edu-meeting.css">
     <link rel="stylesheet" href="assets/css/owl.css">
     <link rel="stylesheet" href="assets/css/lightbox.css">
+
+    <link rel="icon" href="<?= base_url() ?>assets/new.png" type="image/png">
 <!--
 
 TemplateMo 569 Edu Meeting
@@ -114,20 +116,20 @@ https://templatemo.com/tm-569-edu-meeting
           
             <div class="item">
               <div class="down-content">
-                <p>Suspendisse tempor</p>
-                <h4>Best Education</h4>
+                <p>Selamat Datang</p>
+                <h4>di RSUD SYAMRABU</h4>
               </div>
             </div>
             <div class="item">
               <div class="down-content">
-                <p>Suspendisse tempor</p>
-                <h4>Best Education</h4>
+                <p>Semoga Cepat Sembuh</p>
+                <h4>FARMASI RSUD SYAMRABU</h4>
               </div>
             </div>
             <div class="item">
               <div class="down-content">
-                <p>Suspendisse tempor</p>
-                <h4>Best Education</h4>
+                <p>Layanan Pengaduan</p>
+                <h4>0812-8815-1616</h4>
               </div>
             </div>
             
@@ -143,23 +145,23 @@ https://templatemo.com/tm-569-edu-meeting
         <div class="col-lg-4">
         </div>
         <div class="col-lg-4">
-          <div class="right-info">
+          <div class="text-center right-info">
             <ul>
               <li>
-                <h6>Phone Number</h6>
+                <h6>Layanan Informasi</h6>
                 <span>010-020-0340</span>
               </li>
               <li>
-                <h6>Email Address</h6>
-                <span>info@meeting.edu</span>
+                <h6>Email</h6>
+                <span>eres_bangkalan@yahoo.com</span>
               </li>
               <li>
-                <h6>Street Address</h6>
-                <span>Rio de Janeiro - RJ, 22795-008, Brazil</span>
+                <h6>Alamat</h6>
+                <span>Jl. Pemuda Kaffa No.9, Junok, Pejagan, Kec. Bangkalan, Kabupaten Bangkalan, Jawa Timur 69112</span>
               </li>
               <li>
                 <h6>Website URL</h6>
-                <span>www.meeting.edu</span>
+                <span>http://rsabkl.ddns.net/</span>
               </li>
             </ul>
           </div>
@@ -169,7 +171,7 @@ https://templatemo.com/tm-569-edu-meeting
       </div>
     </div>
     <div class="footer">
-      <p>Copyright © 2022 Edu Meeting Co., Ltd. All Rights Reserved. 
+      <p>Creat by © TIM IT | RSUD SYAMRABU BANGKALAN. 
           <br>
           Design: <a href="https://templatemo.com" target="_parent" title="free css templates">TemplateMo</a>
           <br>
